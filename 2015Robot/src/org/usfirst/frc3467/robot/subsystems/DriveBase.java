a139a18d7fb534ec2e71d2df46ac2346ce86df51
ianachin2001 <ianachin2001@gmail.com>
2015-01-17 10:55:29 -0500

    New mecanum code
    stuff

--- 2015Robot/src/org/usfirst/frc3467/robot/subsystems/DriveBase.java
+++ 2015Robot/src/org/usfirst/frc3467/robot/subsystems/DriveBase.java
@@ -1,11 +1,14 @@
 package org.usfirst.frc3467.robot.subsystems;
 
+import org.usfirst.frc3467.robot.OI;
 import org.usfirst.frc3467.robot.RobotMap;
 
+import edu.wpi.first.wpilibj.Gyro;
 import edu.wpi.first.wpilibj.Jaguar;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.command.Subsystem;
+
 
 public class DriveBase extends Subsystem{
 	
@@ -13,14 +16,29 @@ public class DriveBase extends Subsystem{
 	SpeedController backRight = new Jaguar(RobotMap.rearRightMotorPort);
 	SpeedController frontLeft = new Jaguar(RobotMap.frontLeftMotorPort);
 	SpeedController backLeft = new Jaguar(RobotMap.rearLeftMotorPort);
+	static Gyro gyro = new Gyro(0);
 	
 	public static RobotDrive robot = new RobotDrive(RobotMap.frontRightMotorPort,RobotMap.rearRightMotorPort,RobotMap.frontLeftMotorPort,RobotMap.rearLeftMotorPort);
+	static OI oi = new OI();
 	
 	public void initDefaultCommand(){
 		
 	}
 	
-	public void mecanumXYZDrive(double x, double y, double z, double gyro){
-		robot.mecanumDrive_Cartesian(x, y, z, gyro);
+	public void driveMecanum(){
+		double x = 0;
+		double y = 0;
+		double z = 0;
+		if(oi.joystick1.getX() < -.02 || oi.joystick1.getX() > .02){
+			x = oi.joystick1.getX();
+		}
+		if(oi.joystick1.getY() < -.02 || oi.joystick1.getY() > .02){
+			y = oi.joystick1.getY();
+		}
+		if(oi.joystick1.getZ() < -.02 || oi.joystick1.getZ() > .02){
+			z = oi.joystick1.getZ();
+		}
+		
+		robot.mecanumDrive_Cartesian(x, y, z, gyro.getAngle());
 	}
 }
